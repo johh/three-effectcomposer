87f1f5f5fb2b87472eea4de40d321a36dbe8cc8a
JohH <code@johh.net>
2019-04-09 20:11:11 +0200

    TASK: restructured Pass.FullScreenQuad test

--- test/Pass.test.js
+++ test/Pass.test.js
@@ -17,11 +17,6 @@ describe( 'Pass', () => {
 
 					chai.assert.containsAllKeys( B, A );
 
-					const C = new THREE.Pass.FullScreenQuad();
-					const D = new window.EffectComposer.Pass.FullScreenQuad();
-
-					chai.assert.containsAllKeys( C, D );
-
 				} catch ( e ) {
 
 					error = e;
@@ -32,6 +27,38 @@ describe( 'Pass', () => {
 
 			},
 			callback,
+		} );
+
+	} );
+
+	describe( '.FullScreenQuad', () => {
+
+		it( 'should match original in structure', ( callback ) => {
+
+			runTest( {
+				test: () => {
+
+					let error;
+
+					try {
+
+						const A = new THREE.Pass.FullScreenQuad();
+						const B = new window.EffectComposer.Pass.FullScreenQuad();
+
+						chai.assert.containsAllKeys( A, B );
+
+					} catch ( e ) {
+
+						error = e;
+
+					}
+
+					return error;
+
+				},
+				callback,
+			} );
+
 		} );
 
 	} );
