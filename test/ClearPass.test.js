6f9d1fd0a1d09a875a628e020c02ee8eb286b75a
JohH <code@johh.net>
2019-04-09 19:39:43 +0200

    TASK: added functional tests for ClearPass

--- test/ClearPass.test.js
+++ test/ClearPass.test.js
@@ -31,4 +31,112 @@ describe( 'ClearPass', () => {
 
 	} );
 
+
+	describe( 'should produce the same visual results', () => {
+
+		it( 'with no parameters', ( callback ) => {
+
+			runTest( {
+				test: () => {
+
+					let error;
+
+					try {
+
+						const composerA = new THREE.EffectComposer( window.rendererA );
+						composerA.addPass( new THREE.RenderPass(
+							window.scene,
+							window.camera,
+						) );
+
+						const passA = new THREE.ClearPass();
+						composerA.addPass( passA );
+						composerA.render();
+
+
+						const composerB = new window.EffectComposer.default( window.rendererB );
+						composerB.addPass( new window.EffectComposer.RenderPass(
+							window.scene,
+							window.camera,
+						) );
+
+						const passB = new window.EffectComposer.ClearPass();
+						composerB.addPass( passB );
+						composerB.render();
+
+
+						chai.assert.strictEqual(
+							window.canvasA.toDataURL(),
+							window.canvasB.toDataURL(),
+							'results don\'t visually match',
+						);
+
+					} catch ( e ) {
+
+						error = e;
+
+					}
+
+					return error;
+
+				},
+				callback,
+			} );
+
+		} );
+
+
+		it( 'with clear color overwrite', ( callback ) => {
+
+			runTest( {
+				test: () => {
+
+					let error;
+
+					try {
+
+						const composerA = new THREE.EffectComposer( window.rendererA );
+						composerA.addPass( new THREE.RenderPass(
+							window.scene,
+							window.camera,
+						) );
+
+						const passA = new THREE.ClearPass( 0xA2B7E1, 0.5 );
+						composerA.addPass( passA );
+						composerA.render();
+
+
+						const composerB = new window.EffectComposer.default( window.rendererB );
+						composerB.addPass( new window.EffectComposer.RenderPass(
+							window.scene,
+							window.camera,
+						) );
+
+						const passB = new window.EffectComposer.ClearPass( 0xA2B7E1, 0.5 );
+						composerB.addPass( passB );
+						composerB.render();
+
+
+						chai.assert.strictEqual(
+							window.canvasA.toDataURL(),
+							window.canvasB.toDataURL(),
+							'results don\'t visually match',
+						);
+
+					} catch ( e ) {
+
+						error = e;
+
+					}
+
+					return error;
+
+				},
+				callback,
+			} );
+
+		} );
+
+	} );
+
 } );
